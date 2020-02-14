c8569fcc373ad04d7d51b3d89c9eea0e90b0cccd
trallerd <jessicacristec@gmail.com>
2020-02-14 10:11:54 -0300

    Verificação par/impar

--- teste.c
+++ teste.c
@@ -3,6 +3,17 @@
 
 int main(){
 
+    int val;
+    
+    printf("Digite um num. int: ");
+    scanf ("&d", val);
+
+    if(val % 2 == 0){
+        printf("é um número par\n");
+    }
+    else{
+        printf("é um número ímpar\n");
+    }
 
     return 0;
 }
